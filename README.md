156040d29d6bca649ba399ace81bc575489fcf0d
enmanuel <becerracenmanueld@gmail.com>
2024-05-01 02:24:42 -0400

    first release

---
+++ README.md
@@ -0,0 +1,8 @@
+# NODEPP-SQLITE
+Run sqlite in Nodepp
+
+## Dependencies
+- libsqlite3-dev
+
+## Compilation
+`g++ -o main main.cpp -I ./include -lsqlite3 ; ./main`
---
+++ include/sqlite.h
@@ -0,0 +1,70 @@
+#ifndef NODEPP_SQLITE
+#define NODEPP_SQLITE
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+#include <sqlite3.h>
+#include <nodepp/object.h>
+
+namespace nodepp { class sqlite_t {
+protected:
+
+    struct NODE {
+        sqlite3 *fd;
+        int state = 1;
+    };  ptr_t<NODE> obj;
+
+    static int callback( void* data, int argc, char **argv, char **azColName ) { 
+
+        object_t arguments; if( !data ){ return 0; }
+
+        for( auto x=0; x<argc; x++ ) 
+        { arguments[ azColName[x] ] = argv[x] ? argv[x] : "NULL"; }
+        (*type::cast<function_t<void,object_t>>(data))( arguments );
+
+        return 0;
+    }
+
+public:
+    
+    virtual ~sqlite_t() noexcept {
+        if( obj.count() > 1 || obj->fd == nullptr ){ return; }
+        if( obj->state == 0 ){ return; } free();
+    }
+    
+    /*─······································································─*/
+
+    virtual void free() const noexcept {
+        if( obj->fd == nullptr ){ return; }
+        if( obj->state == 0 )   { return; }
+        sqlite3_close( obj->fd );
+        obj->state = 0; 
+    }
+    
+    /*─······································································─*/
+    
+    sqlite_t ( string_t db_file ) : obj( new NODE ) {
+        if( sqlite3_open( db_file.data(), &obj->fd ) ) {
+            process::error( "SQL Error: ", sqlite3_errmsg(obj->fd) );
+        }
+    }
+    
+    /*─······································································─*/
+
+    void exec( const string_t& cmd, const function_t<void,object_t>& cb ) { char* msg;
+        if( sqlite3_exec( obj->fd, cmd.data(), callback, (void*)&cb, &msg) != SQLITE_OK ){
+            process::error( "SQL Error: ", msg ); sqlite3_free( msg );
+        }
+    }
+
+    void exec( const string_t& cmd ) { char* msg;
+        if( sqlite3_exec( obj->fd, cmd.data(), callback, nullptr, &msg) != SQLITE_OK ){
+            process::error( "SQL Error: ", msg ); sqlite3_free( msg );
+        }
+    }
+
+};}
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+#endif
